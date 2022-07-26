305150f3124b67f03fce470cbc0f3d9134b697fb
afnavarro27 <andresfelipenavarrovalencia@gmail.com>
2022-07-25 20:58:33 -0500

    insercion de datos en db

--- planta/models.py
+++ planta/models.py
@@ -1,4 +1,5 @@
 from operator import truediv
+import sre_compile
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,6 @@ class trabajador(models.Model):
     nombre = models.CharField(max_length=200)
     apellido= models.CharField(max_length=200)
     correo=models.EmailField(max_length=250,null=True,blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.nombre} {self.apellido}'
