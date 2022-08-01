0dcd1419a523841c0d9ec509c904b08cfd12c8e0
afnavarro27 <andresfelipenavarrovalencia@gmail.com>
2022-08-01 18:26:28 -0500

    commit tablas

--- planta/admin.py
+++ planta/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import trabajador 
+from .models import Trabajador,Categoria,Produccion,Productos
 # Register your models here.
 
-admin.site.register(trabajador)
+admin.site.register(Trabajador)
+admin.site.register(Categoria)
+admin.site.register(Productos)
+admin.site.register(Produccion)
--- planta/models.py
+++ planta/models.py
@@ -1,10 +1,9 @@
-from operator import truediv
-import sre_compile
+
 from django.db import models
 
 # Create your models here.
 
-class trabajador(models.Model):
+class Trabajador(models.Model):
     cedula = models.IntegerField(max_length=200)
     nombre = models.CharField(max_length=200)
     apellido= models.CharField(max_length=200)
@@ -12,3 +11,31 @@ class trabajador(models.Model):
 
     def __str__(self) -> str:
         return f'{self.nombre} {self.apellido}'
+
+class Categoria(models.Model):
+    nombre=models.CharField(max_length=100)
+    desc=models.CharField(max_length=254)
+
+    
+    def __str__(self) -> str:
+        return f'{self.nombre} '
+
+class Productos(models.Model):
+    nombre=models.CharField(max_length=100,unique=True)
+    ficha=models.TextField()
+    costo=models.IntegerField()
+    categoria=models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
+
+    
+    def __str__(self) -> str:
+        return f'{self.nombre} {self.categoria}'
+
+class Produccion(models.Model):
+    trabajador=models.ForeignKey(Trabajador,on_delete=models.DO_NOTHING)
+    productos=models.ForeignKey(Productos,on_delete=models.DO_NOTHING)
+    cantidad=models.IntegerField()
+    fecha=models.DateTimeField()
+
+    
+    def __str__(self) -> str:
+        return f'{self.trabajador} {self.productos}'
