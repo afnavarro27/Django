9cd992bf4ef78b0958ced072634028b8a3fb9cf4
afnavarro27 <andresfelipenavarrovalencia@gmail.com>
2022-08-01 20:53:08 -0500

    Commit final db, filtros, edit...

--- planta/admin.py
+++ planta/admin.py
@@ -2,7 +2,32 @@ from django.contrib import admin
 from .models import Trabajador,Categoria,Produccion,Productos
 # Register your models here.
 
-admin.site.register(Trabajador)
-admin.site.register(Categoria)
-admin.site.register(Productos)
-admin.site.register(Produccion)
+@admin.register(Trabajador)
+class TrabajadorAdmin(admin.ModelAdmin):
+    list_display= ('id','cedula','nombre','apellido','correo','nombreCompleto')
+    search_fields=['nombre','id','cedula']
+
+
+@admin.register(Productos)
+class ProductosAdmin(admin.ModelAdmin):
+    list_display= ('nombre', 'ficha','costo', 'categoria','color','descriptionCategoria' )
+    list_filter=['categoria']
+    search_fields=['nombre','categoria__nombre']
+    #list_editable=['color']
+
+    def descriptionCategoria(self,obj):
+        return obj.categoria.desc
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display=('id','nombre', 'desc', )
+
+
+@admin.register(Produccion)
+class ProduccionAdmin(admin.ModelAdmin):
+    list_display=('id','trabajador', 'productos', 'cantidad', 'fecha', )
+
+
+
+
+
--- planta/models.py
+++ planta/models.py
@@ -1,4 +1,7 @@
 
+from pyexpat import model
+from statistics import mode
+from turtle import color
 from django.db import models
 
 # Create your models here.
@@ -9,32 +12,42 @@ class Trabajador(models.Model):
     apellido= models.CharField(max_length=200)
     correo=models.EmailField(max_length=250,null=True,blank=True)
 
+    def nombreCompleto(self):
+        return f"{self.nombre} {self.apellido}"
+
     def __str__(self) -> str:
         return f'{self.nombre} {self.apellido}'
 
+    
+
 class Categoria(models.Model):
-    nombre=models.CharField(max_length=100)
-    desc=models.CharField(max_length=254)
+    nombre = models.CharField(max_length=100)
+    desc = models.CharField(max_length=254,null=True,blank=True)
 
     
     def __str__(self) -> str:
         return f'{self.nombre} '
 
 class Productos(models.Model):
-    nombre=models.CharField(max_length=100,unique=True)
-    ficha=models.TextField()
-    costo=models.IntegerField()
-    categoria=models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
-
+    nombre = models.CharField(max_length=100,unique=True)
+    ficha = models.TextField()
+    costo = models.IntegerField()
+    categoria = models.ForeignKey(Categoria ,on_delete=models.DO_NOTHING)
+    COLOR=(
+        ('r','Rojo'),
+        ('v','Verde'),
+        ('a','Azul')
+    )
+    color=models.CharField(max_length=200,choices=COLOR,default='r')
     
     def __str__(self) -> str:
         return f'{self.nombre} {self.categoria}'
 
 class Produccion(models.Model):
-    trabajador=models.ForeignKey(Trabajador,on_delete=models.DO_NOTHING)
-    productos=models.ForeignKey(Productos,on_delete=models.DO_NOTHING)
-    cantidad=models.IntegerField()
-    fecha=models.DateTimeField()
+    trabajador = models.ForeignKey(Trabajador,on_delete=models.DO_NOTHING)
+    productos = models.ForeignKey(Productos,on_delete=models.DO_NOTHING)
+    cantidad = models.IntegerField()
+    fecha = models.DateTimeField()
 
     
     def __str__(self) -> str:
